d822f537df12ab565a0ec6d348059fb99af55b8c
saimomislam26 <saimomislam16@gmail.com>
2023-06-18 15:02:47 +0600

    image folder name change

--- client/src/Component/Services/SpecializedSQA/AutomationTesting.js
+++ client/src/Component/Services/SpecializedSQA/AutomationTesting.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Service from '../Service'
-import servicebanner from '../../../images/automationtesting.png'
+import servicebanner from '../../../image/automationtesting.png'
 const AutomationTesting = () => {
   const title = 'Automation Testing'
   const description = 'With the Digital Revolution, there is stiff competition in every business to deliver the best quality software product, with quick time-to-market. BugRaptors offers premium test automation services to achieve enhanced speed, and impeccable software quality, with maximum coverage.'
